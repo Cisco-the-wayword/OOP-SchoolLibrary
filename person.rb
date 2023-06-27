4839765def3a2e3999599921da9bc2b7aba8a197
Cisco-the-wayword <dexterhack800@gmail.com>
2023-06-27 16:31:18 +0100

    Fix whitespace errors

--- person.rb
+++ person.rb
@@ -7,13 +7,13 @@ class Person
   end
   attr_accessor :name, :age
   attr_reader :id
-  
+
   def can_use_services?
     of_age? || @parent_permission
   end
-  
+
   private
-  
+
   def of_age
     @age >= 18
   end
