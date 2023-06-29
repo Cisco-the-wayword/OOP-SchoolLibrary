3ba7aa40f722aa7a755488ee20413449cab02eec
Cisco <ciscophrenzy@gmail.com>
2023-06-29 14:53:48 -0600

    Update student class

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
 require './person'
 
 class Student < Person
-  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(id, age, name = 'Unknown', parent_permission: true, classroom: nil)
     super(id, age, name, parent_permission)
     @classroom = classroom
+    add_student_to_classroom if classroom
+  end
+
+  def add_student_to_classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
