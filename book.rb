35befc849d4b249d57e04419b0459e42545bf05d
Cisco <ciscophrenzy@gmail.com>
2023-06-29 16:37:19 -0600

    Update book.rb

--- book.rb
+++ book.rb
@@ -7,8 +7,9 @@ class Book
     @rentals = []
   end
 
-  def add_rental(rental)
-    @rentals.push(rental)
-    rental.book = self
+  def add_rental(person, date)
+    rental = Rental.new(date, person, self)
+    person.add_rental(rental)
+    rental
   end
 end
